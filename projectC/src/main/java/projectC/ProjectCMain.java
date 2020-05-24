eaa4b3ce9fc9afd64827e3c6aabc549df18bbbf6
Jingjing Duan <duanjingjing@gmail.com>
2020-05-23 17:07:05 -0700

    make use of guava

--- projectC/src/main/java/projectC/ProjectCMain.java
+++ projectC/src/main/java/projectC/ProjectCMain.java
@@ -1,7 +1,13 @@
 package projectC;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import java.util.List;
+
 class ProjectCMain {
   public static void main(String[] args) {
-    System.out.println("Hello, world! ");
+    List<String> words = Lists.newArrayList("Hello", "world");
+    String greeting = Joiner.on(",").join(words);
+    System.out.println(greeting);
   }
 }
