d989f9c149cd61535705dc2d060626d1f1cd66da
Jingjing Duan <duanjingjing@gmail.com>
2020-05-25 14:04:17 -0700

    make use of guava

--- projectA/src/main/java/projectA/StringUtils.java
+++ projectA/src/main/java/projectA/StringUtils.java
@@ -1,7 +1,12 @@
 package projectA;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import java.util.List;
 
 public class StringUtils {
   public static String concat(String a, String b) {
-    return a + b;
+    List<String> words = Lists.newArrayList(a, b);
+    String greeting = Joiner.on(" ").join(words);
+    return greeting;
   }
 }
--- projectB/src/main/java/projectB/ProjectBMain.java
+++ projectB/src/main/java/projectB/ProjectBMain.java
@@ -4,6 +4,6 @@ import projectA.StringUtils;
 
 class ProjectBMain {
   public static void main(String[] args) {
-    System.out.println("Hello, world! " + StringUtils.concat("Jingjing ", "Duan"));
+    System.out.println("Hello, world! " + StringUtils.concat("Jingjing", "Duan"));
   }
 }
