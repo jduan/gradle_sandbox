e64b955b53a68f881642069e760c37148f922455
Jingjing Duan <duanjingjing@gmail.com>
2020-05-25 14:25:30 -0700

    make projectC use method from projectA

--- projectC/src/main/java/projectC/ProjectCMain.java
+++ projectC/src/main/java/projectC/ProjectCMain.java
@@ -1,13 +1,9 @@
 package projectC;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
-import java.util.List;
+import projectA.StringUtils;
 
 class ProjectCMain {
   public static void main(String[] args) {
-    List<String> words = Lists.newArrayList("Hello", "world");
-    String greeting = Joiner.on(",").join(words);
-    System.out.println(greeting);
+    System.out.println(StringUtils.concat("Hello", "world!"));
   }
 }
